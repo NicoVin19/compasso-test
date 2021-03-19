ba1f1a318d6051272f56e36c5ba28db611ca71fd
NicoVin19 <vincet.nicolas@gmail.com>
2021-03-19 00:29:23 -0300

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          user: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.searchRepos = this.searchRepos.bind(this);
+        this.searchStarred = this.searchStarred.bind(this);
+    
+      }
+      handleChange(e){
+        this.setState(state => ({user: e.target.value}));
+        /*axios.get(`https://api.github.com/users/${this.state.user}`)
+          .then(res => {
+            console.log(res.data); //CHECKING IF USER EXISTS!
+          })
+          .catch(e => {
+            console.log("error");
+          })*/
+    
+      }
+      searchRepos(){
+        axios.get(`https://api.github.com/users/${this.state.user}/repos`)
+          .then(res => {
+              const repos = res.data;
+              const result = document.getElementById('result');
+              result.textContent = '';
+              const reposGrid = document.createElement('ul');
+    
+              for(let repository of repos){
+                const respositoryItem = document.createElement('li');
+                respositoryItem.innerHTML = `<a href="${repository.html_url}" target="_blank" rel="noopener noreferrer">${repository.html_url}</a>`;
+                reposGrid.append(respositoryItem);
+              }
+              result.append(reposGrid);
+    
+          })
+          .catch(e => {
+            const result = document.getElementById('result');
+            result.innerHTML = `<h3>User Not Found!</h3>`;
+          })
+          
+      }
+      searchStarred(){
+        axios.get(`https://api.github.com/users/${this.state.user}/starred`)
+          .then(res => {
+            const repos = res.data;
+            const result = document.getElementById('result');
+            result.textContent = '';
+            const reposGrid = document.createElement('ul');
+            for(let repository of repos){
+              const respositoryItem = document.createElement('li');
+              respositoryItem.innerHTML = `<a href="https://gist.github.com/${repository.owner.login}" target="_blank" rel="noopener noreferrer">${repository.owner.login}</a> - <a href="${repository.html_url}" target="_blank" rel="noopener noreferrer">${repository.html_url}</a>`;
+              reposGrid.append(respositoryItem);
+            }
+            result.append(reposGrid);
+          })
+          .catch(e => {
+              const result = document.getElementById('result');
+              result.innerHTML = `<h3>User Not Found!</h3>`;
+            })
+    
+      }
+      render() {
+        return(
+          <div>
+            <h1>Enter user:</h1>
+            <input type='string' value={this.state.user} onChange={this.handleChange}></input>
+            <button onClick={this.searchRepos}>Repositories</button>
+            <button onClick={this.searchStarred}>Starred</button>
+            <h2>User: {this.state.user}</h2>
+    
+            <div id="result">
+    
+            </div>
+          </div>
+        );
+      }
+    };
 
 export default App;
